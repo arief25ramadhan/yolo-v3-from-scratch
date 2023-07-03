8bfdb8652006f41d97eaec332838916239851733
Arief Ramadhan <arief25ramadhan@yahoo.com>
2023-07-03 22:35:58 +0700

    Update model.py

--- model.py
+++ model.py
@@ -53,9 +53,22 @@ class CNNBlock(nn.Module):
 
     
 class ResidualBlock(nn.Module):
+    def __init__(self, channels, use_residual=True, num_repeats=1):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        for repeat in num_repeats:
+            self.layers += [
+                CNNBlock(channels, channels//2, kernel_size=1),
+                CNNBlock(channels//2, channels, kernel_size=3, padding=1)
+            ]
+            self.use_residual = use_residual
+            self.num_repeats = num_repeats
+
+    def forward(self,x):
+        for layer in self.layers:
+            x = layer(x) + x if self.use_residual else layer(x)
+        return x    
     
-
-
 class ScalePrediction(nn.Module):
     pass
 
